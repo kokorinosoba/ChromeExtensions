3f9386a42b91761f3a2f88fec1749b0e22c9d218
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-09-21 12:16:01 +0900

    :art: リポジトリのページの背景色のみ変更されるようにした

--- Sample/content.js
+++ Sample/content.js
@@ -1,4 +1,6 @@
 window.onload = function() {
-  var body = document.getElementsByClassName('application-main');
-  body[0].style.backgroundColor = '#f0eddb';
+  var is_repository = document.getElementsByClassName('repository-content').length > 0;
+  if (is_repository) {
+    document.getElementsByClassName('logged-in')[0].style.backgroundColor = '#f0eddb';
+  }
 };
